0041b48ce5b1c6983863155ce2a6f265364bbec4
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-22 14:18:04 +0100

    parametrized test

--- src/main/java/pl/sda/PersonValidator.java
+++ src/main/java/pl/sda/PersonValidator.java
@@ -1,8 +1,21 @@
 package pl.sda;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 public class PersonValidator {
 
+    private static Validator validator;
+
     public boolean validate(Person person) {
-        throw new IllegalStateException();
+        initValidator();
+
+        return validator.validate(person, Person.class).size() == 0;
+    }
+
+    private void initValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
 }
