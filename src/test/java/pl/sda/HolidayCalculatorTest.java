10be1fe5a861587f1df7635285ec9b6a05228c57
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-22 12:42:00 +0100

    test

--- src/test/java/pl/sda/HolidayCalculatorTest.java
+++ src/test/java/pl/sda/HolidayCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 
